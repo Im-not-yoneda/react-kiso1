b608e5c3814de1b1d85fa15b86ba29fb561e3d89
米田匠吾 <yoneda.shogo@gmail.com>
2023-10-11 14:51:41 +0900

    スレッド作成ページの作成

---
+++ src/new.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+export const New = () => {
+  const fetchForm = document.querySelector(".fetchForm");
+  //   const submitter = document.querySelector("button[value=save]");
+  const [text, setText] = useState("");
+
+  const makeThread = () => {
+    // const formData = new FormData(fetchForm);
+    fetch(
+      "https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: text }),
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          console.log("error!");
+        }
+        console.log("ok!");
+        return response.json();
+      })
+      .then((data) => {
+        setText("");
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  };
+  return (
+    <div className="new">
+      <h2>スレッド新規作成</h2>
+      <form>
+        <input
+          value={text}
+          onChange={(inputText) => setText(inputText.target.value)}
+          type="text"
+          name="title"
+          placeholder="スレッドタイトル"
+        />
+        <button type="submit" formAction="/" onClick={makeThread}>
+          作成
+        </button>
+      </form>
+      <a href="/">Topに戻る</a>
+    </div>
+  );
+};
+
+export default New;
